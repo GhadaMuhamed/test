fd965c9cf152730d172cbddc0e160c9faa8ba1fa
GhadaSaad <ghadasaad@microsoft.com>
2023-04-12 16:44:52 +0200

    add auth

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        // add get api that returns true
+        [HttpGet]
+        public bool Get()
+        {
+            return true;
+        }
+
+
+
+    }
+}

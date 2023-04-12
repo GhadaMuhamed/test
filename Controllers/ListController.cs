9a2c17642731c7d12e71b8b0ed539836a4d8a391
GhadaSaad <ghadasaad@microsoft.com>
2023-04-12 17:49:13 +0200

    add mails

---
+++ Controllers/ListController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ListController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult ReceiveEmail([FromBody] EmailRequest request)
+        {
+            // Do something with the email address
+            string emailAddress = request.Mail;
+            JsonObject response = new JsonObject();
+            response["mail"] = emailAddress; // set the 'mail' field in the JSON response
+
+            return Ok(response);
+        }
+    }
+
+
+    public class EmailRequest
+    {
+        public string Mail { get; set; }
+    }
+}
